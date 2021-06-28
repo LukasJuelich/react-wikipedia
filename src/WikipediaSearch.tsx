b54c72437763e24f6435da891c532c86be588089
Lukas Jülich <lukas.juelich@rwth-aachen.de>
2021-06-28 10:16:52 +0200

    update npm packages, some refactoring, fix key prop warning

--- src/WikipediaSearch.tsx
+++ src/WikipediaSearch.tsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react'
-import { Text, Stack, TextField, DefaultButton } from 'office-ui-fabric-react'
+import { Text, Stack, TextField, DefaultButton } from '@fluentui/react'
 
 const WikipediaSearch: React.FC = () => {
 	const [ query, setQuery ] = useState("")
 	const [ results, setResults ] = useState<any[]>([])
 
-	var searchWiki = async () => {
+	const searchWiki = async () => {
 		if(query.length <= 3) {
 			setResults(["ERROR: Please enter a longer search term(at least 4 characters)!"]);
 		}
 		else {
-			var wikiURL =	"https://de.wikipedia.org/w/api.php?action=opensearch&search="
-							+ query +
-							"&limit=10&format=json&origin=*";
-			var response = await fetch(wikiURL);
-			var data = await response.json();
+			const wikiURL = 
+				`https://de.wikipedia.org/w/api.php?action=opensearch&search=${query}&limit=10&format=json&origin=*`;
+
+			const response = await fetch(wikiURL);
+			const data = await response.json();
 
 			setResults(formatData(data));
 		}
 	};
 
-	var formatData = (data: [][]) => {
-		var reformattedData = [];
+	const formatData = (data: [][]) => {
+		let reformattedData = [];
 		
-		for(var i = 0; i < data[1].length; i++){
+		for(let i = 0; i < data[1].length; i++){
 			reformattedData.push(
-				<div style={{paddingBottom: "25px"}}>
+				<div key={data[3][i]} style={{paddingBottom: "25px"}}>
 					<a	target="_blank" rel="noopener noreferrer" 
 						href={data[3][i]}
 					>
@@ -49,7 +49,7 @@ const WikipediaSearch: React.FC = () => {
 				<Stack horizontal tokens={{ childrenGap: '0.5rem' }}>
 					<Stack.Item>
 						<TextField	onKeyPress={(event) => { if(event.key === "Enter") searchWiki() }}
-									onChange={(event, newValue) =>{ if(newValue != null)setQuery(newValue) }}
+									onChange={(event, newValue) =>{ if(newValue != null) setQuery(newValue) }}
 									value={query}/>
 					</Stack.Item>
 					<Stack.Item>
